daa45a2276c17c1f24fe945fb7aad0d6698b9e9c
topsv <topsv@hismith.ru>
2022-03-25 20:11:58 +0300

    done

---
+++ components/list/SliderList.vue
@@ -0,0 +1,197 @@
+<template>
+    <v-sheet
+      class="mx-auto"
+      elevation="1"
+      style="background-color: transparent; width: fit-content"
+      
+    >
+      <v-slide-group
+        v-model="model"
+        class="ml-0"
+        center-active
+        show-arrows
+        dark
+      >
+          <v-row justify="center" align="center" class="my-1">
+            <v-slide-item
+              v-slot="{ toggle }"
+              v-for="(card, index) in cards"
+              :key="index"
+              class="ml-1 mr-1 slide-item-card"
+            >
+              <div
+                class="slider-item"
+                v-on:click="toggleCard(card)"
+                @click="toggle"
+              >
+                <div>
+                  <transition name="flip">
+                    <div v-bind:key="card.flipped" class="card">
+                      <img
+                        v-if="!card.flipped"
+                        width="100"
+                        height="100"
+                        :src="card.toolLogo"
+                        alt="logo"
+                      />
+                      <p>
+                        {{
+                          card.flipped ? card.companyName : card.positionTitle
+                        }}
+                      </p>
+                      <a
+                        v-if="card.flipped"
+                        :href="card.flipped ? card.companyLink : null"
+                        >Job Link</a
+                      >
+                      <p>{{ card.flipped ? null : card.jobDate }}</p>
+                    </div>
+                  </transition>
+                </div>
+              </div>
+            </v-slide-item>
+          </v-row>
+      </v-slide-group>
+    </v-sheet>
+</template>
+
+<script>
+export default {
+  name: 'SliderList',
+  props: {
+    cards: {
+      type: Array,
+    },
+  },
+  methods: {
+    toggleCard: function (card) {
+      // if (card.flipped) {
+      //   return null
+      // } else {
+      card.flipped = !card.flipped
+      // }
+    },
+  },
+}
+</script>
+<style>
+ul {
+  padding-left: 0;
+  display: flex;
+  flex-flow: row wrap;
+}
+
+.slide-item {
+  list-style-type: none;
+  padding: 10px 10px;
+  transition: all 10s ease;
+}
+
+.container {
+  max-width: 100%;
+  padding: 2em;
+}
+
+.card {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  flex-wrap: wrap;
+  width: 15rem;
+  height: 15rem;
+  background-color: #51aae5;
+  border-radius: 7px;
+  margin: 5px;
+  cursor: pointer;
+  position: relative;
+  color: #fff;
+  font-weight: 600;
+  font-size: 20px;
+  will-change: transform;
+}
+
+.slider-item:hover {
+  transform: scale(1.1);
+}
+
+.slider-item:nth-child(-n + 3) .card {
+  background-color: #e65f51;
+}
+
+.slider-item:nth-child(2n + 1) .card {
+  background-color: #a17de9;
+}
+
+.slider-item:nth-child(4n) .card {
+  background-color: #feca34;
+}
+
+.slider-item:nth-child(5n-2) .card {
+  background-color: #51aae5;
+}
+
+.slider-item:nth-child(4n + 4) .card {
+  background-color: #feca34;
+}
+
+.slider-item:nth-child(-7n + 7) .card {
+  background-color: #e46055;
+}
+
+.flip-enter-active {
+  transition: all 1s ease;
+}
+
+.flip-leave-active {
+  display: none;
+}
+
+.flip-enter,
+.flip-leave {
+  transform: rotateY(360deg);
+  opacity: 1;
+}
+
+/* Form */
+.flashcard-form {
+  position: relative;
+}
+
+label {
+  font-weight: 400;
+  color: #333;
+  margin-right: 10px;
+}
+
+input {
+  border-radius: 5px;
+  border: 2px solid #eaeaea;
+  padding: 10px;
+  outline: none;
+}
+
+button {
+  border-radius: 5px;
+  border: 1px solid #87cb84;
+  background-color: #87cb84;
+  padding: 8px 15px;
+  outline: none;
+  font-size: 14px;
+  font-weight: 700;
+  color: #fff;
+  cursor: pointer;
+  transition: all 10s ease;
+}
+
+button:hover {
+  background-color: #70a66f;
+}
+
+.error {
+  margin-top: 10px;
+  display: block;
+  color: #e44e42;
+  font-weight: 600;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,79 +1,15 @@
 <template>
   <v-app>
-    <v-sheet class="mx-auto" width="50%" elevation="7">
-      <v-slide-group v-model="model" class="pa-4">
-        <v-slide-item>
-          <v-card
-            :color="active ? 'primary' : 'grey lighten-1'"
-            class="ma-4"
-            height="300"
-            width="300"
-          >
-            <v-row class="fill-height" align="center" justify="center">
-              <h1>slide1</h1>
-            </v-row>
-          </v-card>
-        </v-slide-item>
-        <v-slide-item>
-          <v-card
-            :color="active ? 'primary' : 'grey lighten-1'"
-            class="ma-4"
-            height="300"
-            width="300"
-          >
-            <v-row class="fill-height" align="center" justify="center">
-              <h1>slide2</h1>
-            </v-row>
-          </v-card>
-        </v-slide-item>
-        <v-slide-item>
-          <v-card
-            :color="active ? 'primary' : 'grey lighten-1'"
-            class="ma-4"
-            height="300"
-            width="300"
-          >
-            <v-row class="fill-height" align="center" justify="center">
-              <h1>slide3</h1>
-            </v-row>
-          </v-card>
-        </v-slide-item>
-        <v-slide-item>
-          <v-card
-            class="ma-4"
-            height="300"
-            width="300"
-          >
-            <v-row class="fill-height" align="center" justify="center">
-              <h1>slide4</h1>
-            </v-row>
-          </v-card>
-        </v-slide-item>
-        <v-slide-item>
-          <v-card
-            class="ma-4"
-            height="300"
-            width="300"
-          >
-            <v-row class="fill-height" align="center" justify="center">
-              <h1>slide</h1>
-            </v-row>
-          </v-card>
-        </v-slide-item>
-        <v-slide-item>
-          <v-card
-            :color="active ? 'primary' : 'grey lighten-1'"
-            class="ma-4"
-            height="300"
-            width="300"
-          >
-            <v-row class="fill-height" align="center" justify="center">
-              <h1>slide</h1>
-            </v-row>
-          </v-card>
-        </v-slide-item>
-      </v-slide-group>
-    </v-sheet>
+    <v-container style="height:100%;">
+      <v-row align="center" style="height: 100%;">
+        <v-col class="pl-0 pr-0" align="center">
+          <h1 class="text">Work Experince</h1>
+          <ListSliderList :cards="positions" />
+          <h1 class="mt-5">Pet Projects</h1>
+          <ListSliderList :cards="projects" />
+        </v-col>
+      </v-row>
+    </v-container>
   </v-app>
 </template>
 
@@ -82,13 +18,79 @@ export default {
   name: 'IndexPage',
   data() {
     return {
-      colors: ['red', 'blue', 'green'],
+      positions: [
+        {
+          companyLogo: 'v.png',
+          toolLogo: 'v.png',
+          positionTitle: 'Vue developer',
+          jobDate: '2020-2022',
+          companyName: 'Rshb-intech',
+          companyLink: 'https://google.com',
+          flipped: false,
+        },
+        {
+          companyLogo: 'qa.png',
+          toolLogo: 'qa.png',
+          positionTitle: 'QA Engineer',
+          jobDate: '2018-2020',
+          companyName: 'TechnoProgress',
+          companyLink: 'https://google.com',
+          flipped: false,
+        },
+        {
+          companyLogo: 'v.png',
+          toolLogo: 'qa.png',
+          positionTitle: 'QA Engineer',
+          jobDate: '2017-2018',
+          companyName: 'SberTech',
+          companyLink: 'https://google.com',
+          flipped: false,
+        },
+        {
+          companyLogo: 'v.png',
+          toolLogo: 'qa.png',
+          positionTitle: 'QA Engineer',
+          jobDate: '2016-2017',
+          companyName: 'MTC-communications',
+          companyLink: 'https://google.com',
+          flipped: false,
+        },
+      ],
+      projects: [
+        {
+          toolLogo: 'v.png',
+          positionTitle: 'E-Shop',
+          companyLink: 'https://google.com',
+          flipped: false,
+        },
+        {
+          toolLogo: 'v.png',
+          positionTitle: 'OnlineShop',
+          companyLink: 'https://google.com',
+          flipped: false,
+        },
+        {
+          toolLogo: 'v.png',
+          positionTitle: 'Todo List',
+          companyLink: 'https://google.com',
+          flipped: false,
+        },
+        {
+          toolLogo: 'v.png',
+          positionTitle: 'E-Shop',
+          companyLink: 'https://google.com',
+          flipped: false,
+        },
+      ],
     }
   },
 }
 </script>
 <style>
-.carousel__controls {
-  display: none;
+#app {
+  background-color: #1a1b21;
+}
+h1 {
+  color: #fff;
 }
 </style>
